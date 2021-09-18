52ed8c0545250f4038ea4a399ab34e439381b4e7
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-18 13:14:33 -0700

    added rendering for show route to show drink data, challenge for adding .png links wasn't relevant, as .jpg links were already added in SEIR Ringo code. Manually removed jpg links and programmed .png links in on the show.ejs file instead.

--- models/drinks.js
+++ models/drinks.js
@@ -2,44 +2,43 @@ const drinks = [
     {
       name: 'cruddy mary',
       price: 132,
-      image: 'https://i.imgur.com/Va5iIw5.jpg'
+      image: 'https://i.imgur.com/Va5iIw5'
     },
     {
       name: 'index on the beach',
       price: 68,
-      image: 'https://i.imgur.com/XV2aPa2.jpg'
+      image: 'https://i.imgur.com/XV2aPa2'
     },
     {
       name: 'hack & coke',
       price: 1,
-      image: 'https://i.imgur.com/rLOXFRI.jpg'
+      image: 'https://i.imgur.com/rLOXFRI'
     },
     {
       name: 'whiskey-value pair',
       price: 11,
-      image: 'https://i.imgur.com/a7rmkoS.jpg'
+      image: 'https://i.imgur.com/a7rmkoS'
     },
     {
       name: '404 horsemen',
       price: 202,
-      image: 'https://i.imgur.com/FLucOLr.jpg'
+      image: 'https://i.imgur.com/FLucOLr'
     },
     {
       name: 'if stateMint julep',
       price: 2,
-      image: 'https://i.imgur.com/yaoK0Dg.jpg'
+      image: 'https://i.imgur.com/yaoK0Dg'
     },
     {
       name: 'APIPA',
       price: 43,
-      image: 'https://i.imgur.com/qAhA7pi.jpg'
+      image: 'https://i.imgur.com/qAhA7pi'
     },
     {
       name: 'node to joy',
       price: 56,
-      image: 'https://i.imgur.com/MbVdwZz.jpg'
+      image: 'https://i.imgur.com/MbVdwZz'
     }
   ]
 
   module.exports = drinks
-  
