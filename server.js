c3768c22be07a54c444a8cf92b3cbe33367fbd4c
Nicholas Davis <nickdavis1018@gmail.com>
2021-09-18 12:20:25 -0700

    added drinks index list and capitalized names

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/drinks', (req, res) => {
-    res.render("drinks_index.ejs");
+    res.render("drinks_index.ejs", {allDrinks: drinks});
 });
 
 app.listen(port, () => {
